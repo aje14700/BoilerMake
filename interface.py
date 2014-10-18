e7a1bdd5dc166b4f8ea31bd275887e240a4b7eb7
aje14700 <aj@localhost.localdomain>
2014-10-18 08:53:51 -0400

    added in voice recognition into server class as thread, so program can still exit and be manually changed

--- interface.py
+++ interface.py
@@ -20,6 +20,66 @@ class Server:
         self.connected = True
         return s.accept()
 
+    def voice(self):
+        import pyaudio, wave
+        from array import array
+        from struct import pack
+        import speech_recognition as sr
+        CHUNK_SIZE = 1024
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 1
+        RATE = 16000
+        SILENCE = 3500
+
+
+        p = pyaudio.PyAudio()
+        stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input=True,frames_per_buffer=CHUNK_SIZE)
+        while(this.connected):
+            r = array('h')
+            sound_data = array('h', stream.read(CHUNK_SIZE))
+            while(max(sound_data)<SILENCE):
+                sound_data = array('h', stream.read(CHUNK_SIZE))
+            recording = True
+            silent_count = 0
+            print "recording"
+            while(recording):
+                sound_data = array('h', stream.read(CHUNK_SIZE))
+                r.extend(sound_data)
+                level = max(sound_data)
+                if (level>SILENCE):
+                    silent_count = 0
+                else:
+                    silent_count += 1
+                    if(silent_count>10):
+                        recording = False
+            print "finished recording"
+            r = pack('<' + ('h'*len(r)), *r)
+            wav = wave.open("temp.wav", 'wb')
+            wav.setnchannels(1)
+            wav.setsampwidth(p.get_sample_size(FORMAT))
+            wav.setframerate(RATE)
+            wav.writeframes(r)
+            wav.close()
+            rec = sr.Recognizer()
+            with sr.WavFile("temp.wav") as source:
+                audio = rec.record(source)
+            try:
+                said = str(rec.recognize(audio))
+                if (said=="computer light on"):
+                    self.data[0]='a'
+                elif (said=="computer light off"):
+                    self.data[0]='b'
+                else:
+                    print "Not a command"
+            except LookupError:                                 # speech is unintelligible
+                print("Could not understand audio")
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+
+
+        
     def send(self, sock):
         while(self.connected):
             if(self.data!=self.data_old):
@@ -31,6 +91,8 @@ class Server:
         (sock, (ipclient, ipport)) = self.start()
         send_thread = threading.Thread(target=self.send, args=(sock,))
         send_thread.start()
+        voice_thread = threading.Thread(target=self.voice)
+        voice_thread.start()
         
         while(self.connected):
             ask = raw_input("data value")
